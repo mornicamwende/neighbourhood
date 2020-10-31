45ed2f892945ca7bc941b8d6cd4dd6bdd8b41aa6
Mornica Mwende <mornicamwende@gmail.com>
2020-10-31 18:09:49 +0300

    registering our models

--- neighbourapp/models.py
+++ neighbourapp/models.py
@@ -5,6 +5,29 @@ from django.contrib.auth.models import User
 from django.urls import reverse
 
 # Create your models here.
+class Home(models.Model):
+    name = models.CharField(max_length=255)
+    location = models.CharField(max_length=255)
+    description = models.TextField()
+    Admin = models.ForeignKey(User, on_delete=models.CASCADE, blank=True)
+    pub_date = models.DateTimeField(auto_now_add=True)
+    health = models.IntegerField(null=True, blank=True)
+    police = models.IntegerField(null=True, blank=True)
+
+    def __str__(self):
+        return f'{self.name} mtaa'
+
+    def create_home(self):
+        self.save()
+
+    def delete_home(self):
+        self.delete()
+
+    @classmethod
+    def find_home(cls, home_id):
+        return cls.objects.filter(id=home_id)
+
+
 
 class post(models.Model):
     title = models.CharField(max_length=150)
@@ -12,9 +35,33 @@ class post(models.Model):
     date_posted = models.DateTimeField(default=timezone.now)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     image = models.ImageField(upload_to='posts/')
+    mtaa = models.ForeignKey(Home, on_delete=models.CASCADE,)
+
 
     def __str__(self):
         return self.title
 
     def get_absolute_url(self):
         return reverse('post-detail', kwargs={'pk': self.pk})
+
+
+class Business(models.Model):
+    name = models.CharField(max_length=120)
+    email = models.EmailField(max_length=254)
+    description = models.TextField(blank=True)
+    home = models.ForeignKey(Home, on_delete=models.CASCADE, related_name='business')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='owner')
+    business_image = models.ImageField(upload_to='images/', default='default2.jpg')
+
+    def __str__(self):
+        return f'{self.name} Business'
+
+    def create_business(self):
+        self.save()
+
+    def delete_business(self):
+        self.delete()
+
+    @classmethod
+    def find_business(cls, business_id):
+        return cls.objects.filter(id=business_id)
