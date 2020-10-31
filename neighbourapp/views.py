e3ebcf5f40edeb01dd44378f91d5578247330158
Mornica Mwende <mornicamwende@gmail.com>
2020-10-31 18:14:13 +0300

    registering views

--- neighbourapp/views.py
+++ neighbourapp/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
-from .models import post
+from .forms import HomeForm, postForm, BusinessForm
+from django.contrib.auth.decorators import login_required
+from .models import post, Home, Business
 
 
 def home(request):
@@ -54,6 +56,68 @@ class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
             return False
 
 
-def about(request):
-    return render(request, 'neighbourapp/about.html', {'title': 'About'})
+def index(request):
+    mitaa_zote = Home.objects.all()
+    return render(request, 'neighbourapp/index.html', {'mitaa_zote':mitaa_zote})
 
+def add_mtaa(request):
+    if request.method == 'POST':
+        form = HomeForm(request.POST, request.FILES)
+        if form.is_valid():
+            mtaa = form.save(commit=False)
+            mtaa.admin = request.user.profile
+            mtaa.save()
+            return redirect('index')
+    else:
+        form = HomeForm()
+    return render(request, 'create_mtaa.html', {'form': form})
+
+@login_required(login_url='login')
+def home(request):
+    mitaa_zote = Home.objects.all()
+    mitaa_zote = mitaa_zote[::-1]
+    params = {
+        'mitaa_zote': mitaa_zote,
+    }
+    return render(request, 'mitaa_zote.html', params)
+
+
+def create_post(request, mtaa_id):
+    mtaa = Home.objects.get(id=mtaa_id)
+    if request.method == 'POST':
+        form = postForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.mtaa = mtaa
+            post.user = request.user
+            post.save()
+            return redirect('mtaa', mtaa.id)
+    else:
+        form = postForm()
+    return render(request, 'create_post.html', {'form': form})
+
+
+
+def mtaa(request, mtaa_id):
+    mtaa = Home.objects.get(id=mtaa_id)
+    business = Business.objects.filter(id=mtaa_id)
+    posts = post.objects.filter(id=mtaa_id)
+    if request.method == 'POST':
+        form = BusinessForm(request.POST)
+        if form.is_valid():
+            b_form = form.save(commit=False)
+            b_form.Home = mtaa
+            b_form.user = request.user
+            b_form.save()
+            return redirect('mtaa', mtaa.id)
+    else:
+        form = BusinessForm()
+
+    
+    params = {
+        'mtaa': mtaa,
+        'business': business,
+        'form': form,
+        'posts': posts
+    }
+    return render(request, 'mtaa.html', params)
