0c529100997d5eb629fa85c61b0362da5ab4248f
Mornica Mwende <mornicamwende@gmail.com>
2020-10-31 18:07:58 +0300

    add forms.py file

---
+++ neighbourapp/forms.py
@@ -0,0 +1,22 @@
+from django import forms
+from django.contrib.auth.models import User
+from .models import post, Home, Business 
+from users.models import Profile
+
+
+class HomeForm(forms.ModelForm):
+    class Meta:
+        model = Home
+        exclude = ('admin',)
+
+
+class BusinessForm(forms.ModelForm):
+    class Meta:
+        model = Business
+        exclude = ('user', 'neighbourhood')
+
+
+class postForm(forms.ModelForm):
+    class Meta:
+        model = post
+        exclude = ('user', 'mtaa')
