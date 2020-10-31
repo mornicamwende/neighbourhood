1a48482269c6205aef6f852733a2fca34dbd69b3
Mornica Mwende <mornicamwende@gmail.com>
2020-10-31 18:09:07 +0300

    registering our installed app in setting.py

--- neighbourpro/settings.py
+++ neighbourpro/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'neighbourapp.apps.NeighbourappConfig',
     'users.apps.UsersConfig',
+    'crispy_forms',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -107,7 +108,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Africa/Nairobi'
 
 USE_I18N = True
 
