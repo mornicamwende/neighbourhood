fd01bdb4b2b298471654a33c3037d75f3a482c18
Mornica Mwende <mornicamwende@gmail.com>
2020-11-02 16:50:31 +0300

    add admin.py

--- neighbourapp/admin.py
+++ neighbourapp/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import post
+from .models import post, Home, Business
 
 # Register your models here.
 admin.site.register(post)
+admin.site.register(Home)
+admin.site.register(Business)
