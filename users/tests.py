bf85d7afdb77b059856dc7ecc695cb79d8d1ffd0
Mornica Mwende <mornicamwende@gmail.com>
2020-11-04 16:29:31 +0300

    test

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
+from .models import Profile
+from django.contrib.auth.models import User
 
-# Create your tests here.
+
+class TestProfile(TestCase):
+    def setUp(self):
+        self.user = User(username='Mornica')
+        self.user.save()
+
+        self.profile_test = Profile(user=self.user, image='default.jpg' )
+        
+    def test_instance(self):
+        self.assertTrue(isinstance(self.profile_test, Profile))
+
+    
+    
