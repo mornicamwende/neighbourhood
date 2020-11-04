025b722c092708e0436f8fc0c541ae14823392d6
Mornica Mwende <mornicamwende@gmail.com>
2020-11-04 13:23:04 +0300

    push to heroku

--- neighbourpro/settings.py
+++ neighbourpro/settings.py
@@ -16,7 +16,8 @@ import dj_database_url
 from decouple import config,Csv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
 
 
 # Quick-start development settings - unsuitable for production
